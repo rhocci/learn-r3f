87355e15f11e69788b7196653e44b19397666e18
rhocci <4tres1@gmail.com>
2026-02-23 19:32:27 +0900

    set canvas size

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
-    <div id="canvas-container">
+    <div id="canvas-container" style={{ width: '100vw', height: '100vh' }}>
       <Canvas fallback={<div>WebGL 지원 환경이 아닙니다!</div>}>
-        <mesh>
-          <ambientLight intensity={0.1} />
+        <mesh position={[1, 1, 1]}>
+          <ambientLight intensity={0.9} />
 
-          <boxGeometry args={[5, 5, 5]} />
-          <meshStandardMaterial color="hotpink" />
+          <directionalLight
+            castShadow
+            position={[2.5, 8, 5]}
+            shadow-mapSize={[1024, 1024]}
+          ></directionalLight>
+
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="red" metalness="0.6" roughness="0" />
         </mesh>
       </Canvas>
     </div>
