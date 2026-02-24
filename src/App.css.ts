558d6d2e91ea6ac163ad6feef5850c270a2c8a87
rhocci <4tres1@gmail.com>
2026-02-24 19:37:48 +0900

    set default styles

---
+++ src/App.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const canvasContainer = style({
+  width: '100vw',
+  height: '100vh',
+  background: '#fff',
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Canvas } from '@react-three/fiber';
 import { Experience } from './components/Experience';
+import * as s from './App.css';
 
 export default function App() {
   return (
-    <Canvas>
-      <Experience />
-    </Canvas>
+    <div className={s.canvasContainer}>
+      <Canvas>
+        <Experience />
+      </Canvas>
+    </div>
   );
 }
